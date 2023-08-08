2c1040660de8ccab2cb8002100c2a93dc93a98fa
Tim <timward1985@gmail.com>
2023-08-07 22:00:56 -0500

    colors

--- src/App.jsx
+++ src/App.jsx
@@ -45,23 +45,24 @@ function App() {
 
   return (
     <div>
-      <h1 className="text-3xl font-bold text-center">Get your Todos on yo!!!</h1>
+      <h1 className="text-3xl font-bold text-center mt-7">Get your Todos on yo!!!</h1>
         <div className="flex flex-col border-4 border-black rounded-md px-8 m-auto mt-8 max-w-[70%] md:max-w-[40%] ">
           <TodoList todos={todos} toggleTodo={toggleTodo} />
           <input 
             ref={todoNameRef} 
             type="text" 
-            className="border border-black"
+            placeholder="Type todo here"
+            className="mt-3 border border-black"
           />
           <button 
             onClick={handleAddTodo} 
-            className="border"
+            className="m-auto my-5 border-2 rounded-lg w-24 h-12 font-bold text-black border-blue-600 bg-blue-400 hover:bg-blue-700"
             >Add Todo</button>
           <button 
             onClick={handleClearTodos} 
-            className="border"
+            className="mb-5 rounded-lg w-52 m-auto bg-red-500 font-bold border-red-600 hover:bg-red-700 border"
             >Clear Completed Todos</button>
-          <div>{todos.filter(todo => !todo.complete).length} left to do</div>
+          <div className="font-bold text-center">{todos.filter(todo => !todo.complete).length} left to do.</div>
         </div>
     </div>
   )
